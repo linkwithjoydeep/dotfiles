bbce57f86adbd5f96c469495527e1d2ce6d48d90
Joydeep <linkwithjoydeep@gmail.com>
2024-03-16 19:29:16 +0530

    updated theme to catppuccin

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -1,30 +1,33 @@
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  }
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup {
-  spec = LAZY_PLUGIN_SPEC,
-  install = {
-    colorscheme = { "github", "catppuccin", "default" },
-  },
-  ui = {
-    border = "rounded",
-  },
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    enabled = true,
-    notify = false,
-  },
-}
+require("lazy").setup({
+	spec = LAZY_PLUGIN_SPEC,
+	install = {
+		colorscheme = { --[["github",]]
+			"catppuccin",
+			"default",
+		},
+	},
+	ui = {
+		border = "rounded",
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
+})
