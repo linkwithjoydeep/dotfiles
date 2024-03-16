691732582e3effca16352abe569306979b0f919f
Joydeep <linkwithjoydeep@gmail.com>
2024-03-16 21:07:53 +0530

    fixed typo

--- nvim/lua/user/dap/dap-go.lua
+++ nvim/lua/user/dap/dap-go.lua
@@ -3,15 +3,15 @@ local M = {
 	ft = "go",
 }
 
-M.dependencies = { "mfussenegger/nvim-dap", }
+M.dependencies = { "mfussenegger/nvim-dap" }
 
-M.config = function ()
+M.config = function()
 	local dap_go = require("dap-go")
 
 	dap_go.setup({})
 
 	vim.keymap.set("n", "<leader>dgt", dap_go.debug_test, { desc = "(D)ebug (G)o (T)est" })
-	vim.keymap.set("n", "<leader>dgl", dap_go.debug_last, { desc = "(D)ebug (G)o (L)ast Test" })
+	vim.keymap.set("n", "<leader>dgl", dap_go.debug_last_test, { desc = "(D)ebug (G)o (L)ast Test" })
 end
 
 return M
