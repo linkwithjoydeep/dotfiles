e8a6d633dd12ccc83e697917a2de58daa22e97e6
Joydeep Bhattacharya <linkwithjoydeep@gmail.com>
2025-04-30 22:01:07 +0530

    feat(nvim): switch to snacks explorer

---
+++ .config/nvim/lua/plugins/extend-snacks.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        enabled = false,
+      },
+      picker = {
+        sources = {
+          explorer = {
+            layout = { layout = { position = "right" } },
+          },
+        },
+      },
+    },
+  },
+}
